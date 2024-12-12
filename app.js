bac42eecb2f3ed0f00d2b3512f482bd240b0c826
blovelace84 <blovelace84@gmail.com>
2024-12-12 15:11:26 -0500

    Update all with more questions

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const quizQuestions = [
     {
         question: "What's your favorite activity?",
@@ -9,6 +10,33 @@ const quizQuestions = [
         ]
     },
     //Adding more questions later
+    {
+        question: "What do you value most?",
+        answers: [
+            {text: "Freedom and adventure", character: "Sonic"},
+            {text: "Friendship and loyaty", character: "Tails"},
+            {text: "Strength and Determination", character: "Knuckles"},
+            {text: "Intelligence and Strategy", character: "Dr.Robotnik"}
+        ]
+    },
+    {
+        question: "What motivates you the most?",
+        answers: [
+            {text: "Adventure and excitement", character: "Sonic"},
+            {text: "Innovation and creativity", character: "Tails"},
+            {text: "Strength and Determination", character: "Knuckles"},
+            {text: "Plans and Strategies", character: "Dr. Robotnik"}
+        ]
+    },
+    {
+        question: "How would your friends describe you?",
+        answers: [
+            {text: "Adventurous and free-spirited", character: "Sonic"},
+            {text: "Smart and inventive", character: "Tails"},
+            {text: "Strong and determined", character: "Knuckles"},
+            {text: "Evil and Diabolical", character: "Dr. Robotnik"}
+        ]
+    }
 ];
 
 let currentQuestionIndex = 0;
@@ -17,9 +45,12 @@ const resultsContainer = document.getElementById("results");
 
 function showQuestion(question) {
     quizContainer.innerHTML = `<h2>${question.question}</h2>`;
+
     question.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
+        button.classList.add("answer-choice");
+
         button.onclick = () => selectAnswer(answer.character);
         quizContainer.appendChild(button);
     });
@@ -31,10 +62,10 @@ function selectAnswer(character) {
     if(currentQuestionIndex < quizQuestions.length){
         showQuestion(quizQuestions[currentQuestionIndex]);
     }else{
-        displayResults();
+        displayResults(character);
     }
 }
-function displayResults(){
+function displayResults(character){
     //logic determine and display results can be added here
     resultsContainer.innerHTML = `<h2>You are most like: ${character}</h2>`;
     resultsContainer.classList.remove('hidden');
